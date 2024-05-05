de547495566328c46fe32c7f206ed64c1c87247b
Garleen <garleenn@gmail.com>
2024-05-05 21:03:31 +0300

    КНОПОЧКА

--- client/VUE/testing/src/Quetions.vue
+++ client/VUE/testing/src/Quetions.vue
@@ -123,6 +123,7 @@ export default {
                         <div class="contain" @click="OpenModal">
                             <img src="./assets/add.png" class="add">
                         </div>
+                        <button class="btn find-btn btn-outline-primary text-dark ms-4">Найти</button>
                     </div>
                 </div>
             </div>
@@ -136,6 +137,9 @@ export default {
 </template>
 
 <style scoped>
+.find-btn:hover {
+    color: #fff !important;
+}
 a {
     text-decoration: none;
     color: #000;
