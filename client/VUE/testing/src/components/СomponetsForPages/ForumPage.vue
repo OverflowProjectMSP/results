390e9204bb98ec3058ceff5bac64dc1c899b13fe
Garleen <garleenn@gmail.com>
2024-05-07 23:01:33 +0300

    Запрос на главной форума, правки

---
+++ client/VUE/testing/src/components/СomponetsForPages/ForumPage.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+</script>
+
+<template>
+<div class="container d-flex flex-column gap-2">
+    <h2 class="text-center">Выберите язык:</h2>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=javascript">JavaScript</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=java">Java</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=python">Python</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=php">PHP</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=cpp">C++</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=go">Go</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=ruby">Ruby</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=kotlin">Kotlin</a></button>
+    <button class="btn btn-outline-primary"><a href="#/Forum?lang=typescript">TypeScript</a></button>
+</div>  
+</template>
+
+<style scoped>
+
+</style>
