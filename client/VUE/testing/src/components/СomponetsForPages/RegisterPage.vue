1c24d38e21317271e0517eac9e20d13feb789819
Maxim <justhabber@gmail.com>
2024-05-08 23:32:07 +0300

    Ремастер Регистрации (Картинка, слово)

--- client/VUE/testing/src/components/СomponetsForPages/RegisterPage.vue
+++ client/VUE/testing/src/components/СomponetsForPages/RegisterPage.vue
@@ -86,144 +86,52 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <h1>Регистрация</h1>
-        <form action="#!" @submit.prevent="register">
-            <input class="form-item form-item-1" v-model="nickname" placeholder="Введите никнейм" type="email" name=""
-                id="">
-            <input class="form-item form-item-1" v-model="email" placeholder="Email" type="email" name="" id="">
-            <div class="password">
-                <input class="form-item form-item-2" :type="showPassword" v-model="password" placeholder="Пароль"
-                    type="password" name="password" id="password-input">
-                <img @click="toggleVisibility1" class="password-show" :src="eyeImg1" alt="">
+    <div class="tototocontainer">
+        <div class="background"></div>
+        <div class="content">
+            <p>Регистрация</p>
+            <div class="regist">
+                <form>
+                    <input class="inp inp-username" type="text" name="" id="" placeholder="Никнейм">
+                    <input class="inp inp-email" type="email" placeholder="Почта">
+                </form>
             </div>
-            <div class="exPassword">
-                <input class="form-item form-item-3" :type="showExPassword" v-model="exPassword"
-                    placeholder="Повторите пароль" type="password" name="exPassword" id="exPassword-input">
-                <img @click="toggleVisibility2" class="exPassword-show" :src="eyeImg2" alt="">
-            </div>
-        </form>
-        <div class="error-end-btn">
-            <p class="have_account">Уже есть аккаунт? <a href="#/Login">Войти</a></p>
-            <p class="error">{{ error }}</p>
-            <button type="submit" class="btn-reg" @click="register">Зарегистрироваться</button>
         </div>
     </div>
 </template>
+    
+<style>
+    .tototocontainer {
+        width: 100%;
+        display: flex;
+    }
 
-<style scoped>
-.container {
-    background: #ffffff;
-    width: 400px;
-    height: 580px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 100px;
-    border-radius: 30px;
-    border: solid 3px #2a2a2a;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-}
+    .background {
+        width: 40%;
+        height: 92vh;
+        background: url(../../assets/backgroundUp.png) center;
+        background-repeat: no-repeat; 
+    }
 
-h1 {
-    font-size: 45px;
-    margin-bottom: 50px;
-    user-select: none;
-}
+    .content p {
+        font-size: 38px;
+        margin-bottom: 12px;
+    }
 
-form {
-    display: flex;
-    flex-direction: column;
-}
+    .content {
+        margin-top: 219px;
+        margin-left: 128px;
+    }
 
-.form-item {
-    border: 2px solid #2a2a2a;
-    border-radius: 10px;
-    width: 300px;
-    height: 40px;
+    .regist form {
+        display: flex;
+        flex-direction: column;
+    }
 
-    margin-bottom: 25px;
-    padding-left: 10px;
-    padding-right: 10px;
-}
-
-.form-item:focus {
-    outline: 2px solid #4200FF;
-    border: none;
-}
-
-.btn-reg {
-    margin-top: 10px;
-
-    width: 280px;
-    height: 60px;
-    border-radius: 12px;
-    border: none;
-    background-color: #4200FF;
-    color: #fff;
-    cursor: pointer;
-    font-weight: 600;
-    font-size: 16px;
-
-    transition: all 100ms;
-}
-
-.btn-reg:hover {
-    background-color: #2d00aa;
-    border-radius: 25px 5px;
-}
-
-.btn-reg:active {
-    background-color: #240088;
-}
-
-.error {
-    color: #ff2600;
-    height: 20px;
-    user-select: none;
-}
-
-.have_account {
-    font-size: 20px;
-    user-select: none;
-}
-
-div a {
-    text-decoration: none;
-    color: #4200FF;
-}
+    .inp {
+        width: 700px;
+    }
 
 
-.password {
-    position: relative;
-}
-
-.password-show {
-    position: absolute;
-    cursor: pointer;
-    top: 5px;
-    right: 12px;
-    width: 30px;
-
-}
-
-.exPassword {
-    position: relative;
-}
-
-.exPassword-show {
-    position: absolute;
-    cursor: pointer;
-    top: 5px;
-    right: 12px;
-    width: 30px;
-
-}
-
-
-/* .password-control.view {
-	background: url(../assets/eyeclose.svg) 0 0 no-repeat;
-} */
+    
 </style>
